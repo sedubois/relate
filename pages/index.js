10dfed0b02d62773b6829186d4f1ab0ed56451d1
Sebastien Dubois <sdubois@protonmail.com>
2016-11-07 18:03:59 +0200

    Add GitHub corner ribbon

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import GitHubRibbon from '../components/GitHubRibbon';
 import page from '../hocs/page';
 
-export default page(() => <div>Welcome!</div>);
+export default page(() => <div>
+  <GitHubRibbon />
+  Welcome!
+</div>);
