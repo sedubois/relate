8d1c9ffcc7057a1181ce42230514d115da399af0
Sebastien Dubois <sdubois@protonmail.com>
2017-01-16 18:17:16 +0200

    Fix server-rendering of static pages

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 const route = pathMatch();
+const staticMatch = route('/(discover|about)');
 const profileMatch = route('/:slug');
 const trackMatch = route('/track/:id');
 
@@ -16,20 +17,21 @@ app.prepare()
       const { pathname } = parse(req.url);
       // leave untouched routes like /__webpack_hmr or /_next/-/pages/
       const hasUnderscore = pathname[1] && pathname[1] === '_';
-      if (hasUnderscore) {
+      if (hasUnderscore || staticMatch(pathname)) {
+        // fall-through
         handle(req, res);
-        return;
-      }
-
-      const profileParams = profileMatch(pathname);
-      if (profileParams !== false) {
-        app.render(req, res, '/profile', profileParams);
         return;
       }
 
       const trackParams = trackMatch(pathname);
       if (trackParams !== false) {
         app.render(req, res, '/track', trackParams);
+        return;
+      }
+
+      const profileParams = profileMatch(pathname);
+      if (profileParams !== false) {
+        app.render(req, res, '/profile', profileParams);
         return;
       }
 
