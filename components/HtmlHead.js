645f0d658a678caa974ade0c057f3f4878c024eb
Sebastien Dubois <sdubois@protonmail.com>
2017-01-17 16:14:29 +0200

    Add page title

--- components/HtmlHead.js
+++ components/HtmlHead.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 
 const HtmlHead = () => (
   <Head>
+    <title>Relate</title>
     <meta charSet="utf-8" />
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" />
