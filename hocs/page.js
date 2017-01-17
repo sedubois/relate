50dc3f317eb3efa045d5bbe40d1a1a1ad1116458
Sebastien Dubois <sdubois@protonmail.com>
2017-01-17 15:06:36 +0200

    Add analytics with autotrack

--- hocs/page.js
+++ hocs/page.js
@@ -1,57 +1,63 @@
-import { PropTypes } from 'react';
+import { Component, PropTypes } from 'react';
+import { configureAnalytics, pageView } from '../util/analytics';
 import configureProgressBar from '../util/routing';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import HtmlHead from '../components/HtmlHead';
 import withData from './withData';
 
+configureAnalytics();
 configureProgressBar();
 
 // color palette: http://paletton.com/#uid=33m0y0ksMDf8jVahZJZEepkKleL
 
-const page = (WrappedComponent) => {
-  const Page = props => (
-    <div className="wrapper">
-      <style jsx>{`
-      .wrapper {
-        display: flex;
-        flex-direction: column;
-        font-family: Raleway, "Helvetica Neue", Helvetica, Arial, sans-serif;
-        font-size: 85%;
-        margin: 0 auto;
-        max-width: 960px;
-        min-height: 100%;
-        padding: 0 16px;
-      }
-    `}</style>
-      <style jsx global>{`
-      a {
-        color: #02697C;
-        text-decoration: none;
-      }
-
-      body {
-        margin: 0;
-      }
-
-      .auth0-lock-header {
-        display: none;
-      }
-    `}</style>
-      <HtmlHead />
-      <Header url={props.url} />
-      <WrappedComponent {...props} />
-      <Footer />
-    </div>
-  );
-
-  Page.propTypes = {
+const page = WrappedComponent => class Page extends Component {
+  static propTypes = {
     url: PropTypes.shape({
       pathname: PropTypes.string.isRequired,
     }).isRequired,
   };
 
-  return Page;
+  componentWillMount() {
+    pageView();
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <style jsx>{`
+        .wrapper {
+          display: flex;
+          flex-direction: column;
+          font-family: Raleway, "Helvetica Neue", Helvetica, Arial, sans-serif;
+          font-size: 85%;
+          margin: 0 auto;
+          max-width: 960px;
+          min-height: 100%;
+          padding: 0 16px;
+        }
+      `}</style>
+        <style jsx global>{`
+        a {
+          color: #02697C;
+          text-decoration: none;
+        }
+
+        body {
+          margin: 0;
+        }
+
+        .auth0-lock-header {
+          display: none;
+        }
+      `}</style>
+        <HtmlHead />
+        <Header url={this.props.url} />
+        <WrappedComponent {...this.props} />
+        <Footer />
+      </div>
+    );
+  }
 };
 
 export default WrappedComponent => withData(page(WrappedComponent));
---
+++ util/analytics.js
@@ -0,0 +1,29 @@
+import { ANALYTICS_TRACKING_ID } from '../config';
+import { IS_BROWSER } from '../util/website';
+
+export function configureAnalytics() {
+  if (IS_BROWSER) {
+    // eslint-disable-next-line
+    window.ga = window.ga || function () {(ga.q = ga.q || []).push(arguments)};ga.l = +new Date;
+    /* global ga: true */
+    // ga comes from google-analytics script injected below
+    ga('create', ANALYTICS_TRACKING_ID, 'auto');
+    ga('require', 'urlChangeTracker');
+    /* eslint-disable global-require */
+    require('autotrack/lib/plugins/clean-url-tracker');
+    ga('require', 'cleanUrlTracker');
+    require('autotrack/lib/plugins/outbound-form-tracker');
+    ga('require', 'outboundFormTracker');
+    require('autotrack/lib/plugins/outbound-link-tracker');
+    /* eslint-enable global-require */
+    ga('require', 'outboundLinkTracker');
+  }
+}
+
+export function pageView() {
+  if (IS_BROWSER) {
+    const page = window.location.pathname;
+    ga('set', 'page', page);
+    ga('send', 'pageview');
+  }
+}
