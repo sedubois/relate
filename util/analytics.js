a9d2bce46e5314075af7c12bbaa0266865ff25da
Sebastien Dubois <sdubois@protonmail.com>
2017-01-18 14:00:40 +0200

    Switch from autotrack to react-ga to get analytics working with next.js

--- util/analytics.js
+++ util/analytics.js
@@ -1,28 +1,21 @@
 import { ANALYTICS_TRACKING_ID } from '../config';
 import { IS_BROWSER } from '../util/website';
 
+let ReactGA;
+if (IS_BROWSER) {
+  ReactGA = require('react-ga'); // eslint-disable-line global-require
+}
+
 export function configureAnalytics() {
   if (IS_BROWSER) {
-    // eslint-disable-next-line
-    window.ga = window.ga || function () {(ga.q = ga.q || []).push(arguments)};ga.l = +new Date;
-    /* global ga: true */
-    ga('create', ANALYTICS_TRACKING_ID, 'auto');
-    ga('require', 'urlChangeTracker');
-    /* eslint-disable global-require */
-    require('autotrack/lib/plugins/clean-url-tracker');
-    ga('require', 'cleanUrlTracker');
-    require('autotrack/lib/plugins/outbound-form-tracker');
-    ga('require', 'outboundFormTracker');
-    require('autotrack/lib/plugins/outbound-link-tracker');
-    /* eslint-enable global-require */
-    ga('require', 'outboundLinkTracker');
+    ReactGA.initialize(ANALYTICS_TRACKING_ID);
   }
 }
 
 export function pageView() {
   if (IS_BROWSER) {
     const page = window.location.pathname;
-    ga('set', 'page', page);
-    ga('send', 'pageview');
+    ReactGA.set({ page });
+    ReactGA.pageview(page);
   }
 }
