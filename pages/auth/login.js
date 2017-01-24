d08982b5206803d2f393063469ba8bbf76f8c677
Sebastien Dubois <sdubois@protonmail.com>
2017-01-24 12:00:50 +0200

    fix typo

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -41,7 +41,7 @@ class Login extends Component {
   };
 
   async componentDidMount() {
-    if (!this.props.loggedIn) {
+    if (this.props.loggedIn) {
       this.props.url.replace('/');
     } else {
       createAndShow('/');
