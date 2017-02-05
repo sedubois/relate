bfe906d500c3e4a4f7cf31264b3fa2c21cbe784b
Sebastien Dubois <sdubois@protonmail.com>
2017-02-05 16:04:50 +0200

    Pass locale to login flow (TODO: use it in callback)

--- components/Lock.js
+++ components/Lock.js
@@ -7,44 +7,56 @@ import { storeSecret, removeSecret } from '../util/authSecret';
 
 const LOCK_CONTAINER_ID = 'lock-container';
 
-function createLock(nextPathname) {
-  const secret = uuid.v4();
-  storeSecret(secret);
-  const lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN, {
-    auth: {
-      redirectUrl: `${BASE_URL}/auth/callback`,
-      responseType: 'token',
-      params: {
-        state: JSON.stringify({
-          secret,
-          nextPathname,
-        }),
-      },
-    },
-    container: LOCK_CONTAINER_ID,
-    // other options see https://auth0.com/docs/libraries/lock/v10/customization
-  });
-  lock.show();
-}
-
 export default class Lock extends Component {
   static propTypes = {
     url: PropTypes.object.isRequired,
     auth: PropTypes.shape({
       loggedIn: PropTypes.bool.isRequired,
     }).isRequired,
+    locale: PropTypes.string.isRequired,
   };
 
   componentDidMount() {
+    const nextPathname = '/';
     if (this.props.auth.loggedIn) {
-      this.props.url.replace('/');
+      this.props.url.replace(nextPathname);
     } else {
-      createLock('/');
+      this.lockState.locale = this.props.locale;
+      this.showLock();
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.locale !== this.props.locale) {
+      this.lockState.locale = nextProps.locale;
+      this.showLock();
     }
   }
 
   componentWillUnmount() {
     removeSecret();
+  }
+
+  lockState = { nextPathname: '/' };
+
+  showLock() {
+    const secret = uuid.v4();
+    storeSecret(secret);
+    const lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN, {
+      auth: {
+        redirectUrl: `${BASE_URL}/auth/callback`,
+        responseType: 'token',
+        params: {
+          state: JSON.stringify({
+            secret,
+            ...this.lockState,
+          }),
+        },
+      },
+      container: LOCK_CONTAINER_ID,
+      // other options see https://auth0.com/docs/libraries/lock/v10/customization
+    });
+    lock.show();
   }
 
   render() {
--- pages/auth/login.js
+++ pages/auth/login.js
@@ -3,9 +3,9 @@ import NoSSR from 'react-no-ssr';
 import Lock from '../../components/Lock';
 import page from '../../hocs/page';
 
-const Login = ({ auth, url }) => (
+const Login = ({ auth, url, locale }) => (
   <NoSSR>
-    <Lock auth={auth} url={url} />
+    <Lock auth={auth} url={url} locale={locale} />
   </NoSSR>
 );
 
@@ -14,6 +14,7 @@ Login.propTypes = {
   auth: PropTypes.shape({
     loggedIn: PropTypes.bool.isRequired,
   }).isRequired,
+  locale: PropTypes.string.isRequired,
 };
 
 export default page(Login);
