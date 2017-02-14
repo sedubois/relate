8ea8ae2dd70c5203e2563091c263057d4eadd767
Sebastien Dubois <sdubois@protonmail.com>
2017-02-14 10:51:50 +0200

    Add missing routes not handled by next-routes

--- universal/routes.js
+++ universal/routes.js
@@ -1,6 +1,8 @@
 const nextRoutes = require('next-routes');
 
 const routes = nextRoutes();
+routes.add('about', '/about');
+routes.add('discover', '/discover');
 routes.add('retreat', '/retreat/:id');
 routes.add('track', '/track/:id');
 routes.add('profile', '/:slug');
