368ad2e3c0ab4c07c56160147d4b6544772ec5fa
Sebastien Dubois <sdubois@protonmail.com>
2017-02-14 17:26:43 +0200

    add webpack-bundle-analyzer in dev mode

--- next.config.js
+++ next.config.js
@@ -1,9 +1,23 @@
 /* eslint-disable no-param-reassign */
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
 module.exports = {
-  webpack: (config) => {
+  webpack: (config, { dev }) => {
     config.externals = {
       fs: 'fs',
     };
+
+    if (dev) {
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          // For all options see https://github.com/th0r/webpack-bundle-analyzer#as-plugin
+          analyzerMode: 'server',
+          analyzerHost: '127.0.0.1',
+          analyzerPort: 8888,
+          openAnalyzer: false,
+        }));
+    }
+
     return config;
   },
 };
