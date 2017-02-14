28a1059fd8a72d5e75eeefe1b867c4ae510022e7
Sebastien Dubois <sdubois@protonmail.com>
2017-02-14 11:51:40 +0200

    Add route for the home page

--- universal/routes.js
+++ universal/routes.js
@@ -1,6 +1,7 @@
 const nextRoutes = require('next-routes');
 
 const routes = nextRoutes();
+routes.add('index', '/');
 routes.add('about', '/about');
 routes.add('discover', '/discover');
 routes.add('retreat', '/retreat/:id');
